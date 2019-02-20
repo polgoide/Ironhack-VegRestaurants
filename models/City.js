72f1129c05b7c86c4e0a6912b810011a5b459577
Daniela Lopez <danielalopez@MacBook-Air-de-Daniela.local>
2019-02-20 09:21:19 -0600

    Estilos en login y signup

--- models/City.js
+++ models/City.js
@@ -8,11 +8,10 @@ let citySchema = new Schema({
   },
   photoURL: {
     type:String,
-    default: 'https://www.worldatlas.com/r/w727-h434-c727x434/upload/4f/df/5e/shutterstock-501836233.jpg'
+    default: '/images/alimentos-vegetarianos.jpg'
   },
-  likes:{
+  info:{
     type:String,
-    default: '0'
   },address: {
     location: {
       type: String,
--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,5 +1,15 @@
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener('DOMContentLoaded', function() {
+
+  var instance = M.Carousel.init({
+    fullWidth: true
+  })
+  var pics = document.querySelectorAll('.materialboxed');
+  var picInstances = M.Materialbox.init(pics, );
+});
+
+
+
+  
 
   console.log('IronGenerator JS imported successfully!');
 
-}, false);
