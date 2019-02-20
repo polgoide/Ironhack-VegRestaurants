49d700c3db92ade83bfcfc04740dfb3a437da6e2
Daniela Lopez <danielalopez@MacBook-Air-de-Daniela.local>
2019-02-20 12:46:36 -0600

    imágenes corregidas de places y cities

--- app.js
+++ app.js
@@ -65,7 +65,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
 
 // default value for title local
-app.locals.title = 'Express - Generated with IronGenerator';
+app.locals.title = '';
 app.locals.logged = false
 app.locals.showMaterialize = true
 
